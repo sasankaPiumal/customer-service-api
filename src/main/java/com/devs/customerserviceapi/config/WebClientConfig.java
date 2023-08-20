0637ed5c23564c5476c915bd6d282f3ef897b173
sasanka <Sasanka@DESKTOP-84NE784.(none)>
2023-08-20 06:07:29 +0530

    WebClientConfig updated.

--- src/main/java/com/devs/customerserviceapi/config/WebClientConfig.java
+++ src/main/java/com/devs/customerserviceapi/config/WebClientConfig.java
@@ -1,4 +1,18 @@
 package com.devs.customerserviceapi.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
 public class WebClientConfig {
+
+    @Value("${address.service.url}")
+    private String addressServiceUrl;
+
+    public WebClient webClient() {
+        WebClient webClient = WebClient.builder().baseUrl(addressServiceUrl).build();
+        return webClient;
+    }
+
 }
