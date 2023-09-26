a1b1f2478df589fb27e8d7417b1e1cc788cb2a0a
sasanka <Sasanka@DESKTOP-84NE784.(none)>
2023-09-26 13:48:57 +0530

    security added.

--- src/main/java/com/devs/customerserviceapi/security/ApplicationSecurityConfig.java
+++ src/main/java/com/devs/customerserviceapi/security/ApplicationSecurityConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.List;
 
 
 @Configuration
@@ -12,8 +15,24 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+//        super.configure(http);
 
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedHeaders(List.of("Authorization","Cache-Control","Content-Type"));
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.setAllowedMethods(List.of("POST","DELETE","GET","PUT","PATCH","OPTION"));
+        corsConfiguration.setAllowCredentials(false);
+        corsConfiguration.setExposedHeaders(List.of("Authorization"));
+
+        http.csrf().disable()
+                .cors().configurationSource(request -> corsConfiguration)
+                .and()
+                .authorizeHttpRequests()
+                .antMatchers(
+                        "/api/v1/users/register/**"
+                ).permitAll()
+                .anyRequest()
+                .permitAll();
 
     }
 }
