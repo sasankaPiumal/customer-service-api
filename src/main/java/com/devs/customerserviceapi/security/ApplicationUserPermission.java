3c0b192fc65fdaab917bfeec5d4152ba5f11efb0
sasanka <Sasanka@DESKTOP-84NE784.(none)>
2023-09-26 14:49:39 +0530

    appication user enum permission added.

---
+++ src/main/java/com/devs/customerserviceapi/security/ApplicationUserPermission.java
@@ -0,0 +1,22 @@
+package com.devs.customerserviceapi.security;
+
+public enum ApplicationUserPermission {
+
+    CUSTOMER_READ("customer:read"),
+
+    CUSTOMER_WRITE("customer:write"),
+
+    PRODUCT_READ("product:read"),
+
+    PRODUCT_WRITE("product:write");
+
+    private final String permission;
+
+    ApplicationUserPermission(String permission) {
+        this.permission = permission;
+    }
+
+    public String getPermission(){
+        return permission;
+    }
+}
